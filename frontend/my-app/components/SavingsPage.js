20e2018f3632c541d053ebf7d708ef9f9dcad31e
agni.ramadani <agni.ramadani@unibas.ch>
2022-09-17 20:52:38 +0200

    Add frontend after being removed

---
+++ frontend/my-app/components/SavingsPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {Text, ScrollView} from "react-native";
+
+export default function SavingsPage(){
+
+    return <ScrollView>
+        <Text>
+            Savings Page!
+        </Text>
+    </ScrollView>
+};
---
+++ frontend/my-app/components/TrendsPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {Text, ScrollView} from "react-native";
+
+export default function TrendsPage(){
+
+    return <ScrollView>
+        <Text>
+            Ola
+        </Text>
+    </ScrollView>
+};
